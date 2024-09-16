94f06cad29cdc760e8963bf191add45c381b2ffa
Gábor Torma <gabor@torma.dev>
2024-09-16 18:26:12 +0200

    fix: add lint for playground

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
-    My playground
-  </div>
-  <div>
-    <a href="/hello">
-      Go to Hello
-    </a>
-  </div>
-  <div>
-    <a href="/chat">
-      Go to Chat
-    </a>
-  </div>
-  <div>
-    <a href="/ws">
-      Go to WebSocket
-    </a>
-  </div>
-  <div>
-    <a href="/express">
-      Go to Express
-    </a>
+    <div>
+      My playground
+    </div>
+    <div>
+      <a href="/hello">
+        Go to Hello
+      </a>
+    </div>
+    <div>
+      <a href="/chat">
+        Go to Chat
+      </a>
+    </div>
+    <div>
+      <a href="/ws">
+        Go to WebSocket
+      </a>
+    </div>
+    <div>
+      <a href="/express">
+        Go to Express
+      </a>
+    </div>
   </div>
 </template>
 
