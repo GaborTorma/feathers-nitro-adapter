12fc0274bfcbeef7c1ac80a16432bcfa07c76608
Gábor Torma <gabor@torma.dev>
2024-10-04 14:45:44 +0200

    fix: remove unused Koa instance from Koa test fixture

--- test/fixtures/koa/feathers-api/src/app.ts
+++ test/fixtures/koa/feathers-api/src/app.ts
@@ -1,13 +1,10 @@
 import type { Application } from './declarations'
 import { feathers } from '@feathersjs/feathers'
 import { bodyParser, koa as feathersKoa, rest, serveStatic } from '@feathersjs/koa'
-import Koa from 'koa'
 import { dummy } from './dummy'
 import { services } from './services/index'
 
-export const koa = new Koa()
-
-export const app: Application = feathersKoa(feathers(), koa)
+export const app: Application = feathersKoa(feathers())
 
 app.use(bodyParser())
 
