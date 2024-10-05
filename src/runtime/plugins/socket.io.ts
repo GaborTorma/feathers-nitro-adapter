b3df55fc9888337e16c489dc76cc902df52cd5de
Gábor Torma <gabor@torma.dev>
2024-10-05 09:57:50 +0200

    fix: support crossws 0.2 and 0.3

--- src/runtime/plugins/socket.io.ts
+++ src/runtime/plugins/socket.io.ts
@@ -1,8 +1,9 @@
 import type { Application as FeathersApplication } from '@feathersjs/feathers'
 import type { NitroApp, NitroAppPlugin } from 'nitropack'
 import type { IncomingMessage } from 'node:http'
-import type { Server as SocketServer } from 'socket.io'
-import {} from '@feathersjs/socketio' // need for declaration merging
+import type { Socket } from 'node:net'
+import type { Server as SocketServer } from 'socket.io' // need for declaration merging
+import {} from '@feathersjs/socketio'
 import { Server as EngineServer } from 'engine.io'
 import { defineEventHandler } from 'h3'
 import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin'
@@ -10,17 +11,20 @@ import { setup } from '../setup'
 
 type EngineRequest = Parameters<EngineServer['handleRequest']>[0]
 
-interface NodePeer {
-  req: IncomingMessage
-  ws: WebSocket
+interface CrossWs03Peer {
+  request: {
+    _req: IncomingMessage
+  }
+  websocket: WebSocket
 }
 
-interface NodeContext<Peer> {
-  node: Peer
-}
-
-interface Peer<Context> {
-  ctx: Context
+interface CrossWs02Peer {
+  ctx: {
+    node: {
+      req: IncomingMessage
+      ws: WebSocket
+    }
+  }
 }
 
 export async function createSocketIoRouter(feathersApp: FeathersApplication) {
@@ -36,17 +40,35 @@ export async function createSocketIoRouter(feathersApp: FeathersApplication) {
         event._handled = true
       },
       websocket: {
-        open(peer: Peer<NodeContext<NodePeer>>) {
-          const nodeContext = peer.ctx.node
+        open(peer: CrossWs02Peer | CrossWs03Peer) {
+          let req: IncomingMessage
+          let websocket: WebSocket
+          let rawSocket: Socket
 
-          const req = nodeContext.req
+          if ((peer as CrossWs02Peer).ctx) {
+            // original peer format in crossws ^0.2
+            // this version used in unit test
+            const peer02 = peer as CrossWs02Peer
+            const nodeContext = peer02.ctx.node
+            req = nodeContext.req
+            rawSocket = nodeContext.req.socket
+            websocket = nodeContext.ws
+          }
+          else if ((peer as CrossWs03Peer).request) {
+            // new peer format in crossws ^0.3
+            // more info: https://github.com/unjs/crossws/releases/tag/v0.3.0
+            const peer03 = peer as CrossWs03Peer
+            req = peer03.request._req
+            rawSocket = req.socket
+            websocket = peer03.websocket
+          }
+          else {
+            throw new Error('Unknown peer type')
+          }
 
           // @ts-expect-error private method
           // eslint-disable-next-line ts/no-unsafe-call
           io.engine.prepare(req)
-
-          const rawSocket = nodeContext.req.socket
-          const websocket = nodeContext.ws
 
           // @ts-expect-error private method
           // eslint-disable-next-line ts/no-unsafe-call
